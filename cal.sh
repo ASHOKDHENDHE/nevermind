9f0057bbbdc0e2f580b211e8743763a8faa66136
Ashok Dhendhe <66132686+ASHOKDHENDHE@users.noreply.github.com>
2022-10-10 22:10:33 +0530

    Calculation (addition,subtraction,multiplication and modulus)

---
+++ cal.sh
@@ -0,0 +1,18 @@
+echo "Enter first number"
+x=50
+echo "Enter second number"
+y=30
+(( sum=x+y ))
+echo "The result of addition=$sum"
+
+#Subtraction
+(( sub=x-y ))
+echo "The result of subtraction=$sub"
+
+#Subtraction
+(( mul=x*y ))
+echo "The result of subtraction=$mul"
+
+#Subtraction
+(( mod=x%y ))
+echo "The result of subtraction=$mod"
